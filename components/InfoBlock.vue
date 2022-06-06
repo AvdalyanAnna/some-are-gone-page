7cbb64bb0b10ba51d7a1161f686a56182595b074
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-06-06 15:47:40 +0400

    init

---
+++ components/InfoBlock.vue
@@ -0,0 +1,89 @@
+<template>
+  <section class="info">
+    <div class="wrapper">
+      <div class="info__inner">
+
+        <p class="description">
+          Если вы держите в руках данную бутылку, знайте — у вас очень хорошие друзья!
+          <br>
+          Иначе, как объяснить попадание на ваш стол нашего напитка?</p>
+        <p class="description">
+          Данный сайт мы разработали для ознакомительных целей. Здесь вы найдёте всю информацию о
+          составе и технологии производства.
+          <br>
+          А в превосходном качестве нашего продукта вы сможете убедиться после дегустации.</p>
+        <p class="description"> Напоминаем, что чрезмерное употребление алкоголя вредит вашему здоровью.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "InfoBlock"
+}
+</script>
+
+<style lang="scss" scoped>
+.info {
+  background-image: url('@/assets/images/info_bg.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  padding: 130px 0;
+  box-sizing: border-box;
+  position: relative;
+
+  &:after,
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100px;
+    height: 18px;
+    background-image: url('@/assets/images/info_effect.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &:before {
+    top: 80px;
+  }
+
+  &:after {
+    bottom: 80px;
+    transform: translateX(-50%) rotate(180deg);
+  }
+
+  &__inner {
+    .title {
+      margin-bottom: 20px;
+    }
+    .description{
+      margin-bottom: 10px;
+      &:nth-last-child(1){
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+@media screen and (max-width: 550px) {
+  .info {
+    padding: 100px 0;
+    &:before {
+      top: 55px;
+    }
+
+    &:after {
+      bottom: 55px;
+    }
+    .description{
+      width: 95%;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Some Are Gone',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', sizes:'36x36', href: '/36.png' },
+      { rel: 'icon', type: 'image/png', sizes:'48x48', href: '/48.png' },
+      { rel: 'icon', type: 'image/png', sizes:'72x72', href: '/72.png' },
+      { rel: 'icon', type: 'image/png', sizes:'96x96', href: '/96.png' },
+      { rel: 'icon', type: 'image/png', sizes:'144x144', href: '/144.png' },
+      { rel: 'icon', type: 'image/png', sizes:'192x192', href: '/192.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/moonshine_elite_vintage.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/moonshine_elite_vintage-gallery.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/zubrovka.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/zubrovka-gallery.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/rum.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/rum-gallery.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/premium_quality_rum.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/premium_quality_rum-gallery.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/chacha.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/chacha-gallery.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/gin.png' },
+      { rel: 'preload', type: 'image/png',  href: '/products/gin-gallery.png' },
+    ]
+  },
+
+  styleResources: {
+    scss: [
+      '~/assets/scss/_fonts.scss',
+      '~/assets/scss/_mixin.scss',
+      '~/assets/scss/main.scss'
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    {src: '~/assets/scss/main.scss', lang: 'scss'},
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/style-resources'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
